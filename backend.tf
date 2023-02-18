e6efae522b7b0ffc425b8038832e99680d2c1b68
unknown <bala.dendukuri@gmail.com>
2023-02-18 16:19:47 +0530

    fjdlfdsaflkajs

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 # Sepficy the GCP Provider
 resource "google_storage_bucket" "storage-34345676" {
   name = "storage-34345676"
-  project = "gcptrainproject"
-  region = "us-central1-a"
+#  project = "gcptrainproject"
+#  region = "us-central1-a"
 }
